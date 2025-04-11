9d00d981e1e2b78f2139bb2d860aa7ef2ef104d1
delvauxo <delvauxo@outlook.com>
2025-04-11 18:12:50 +0200

    auth: add unauthorized access denied page

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,8 @@
+export default function UnauthorizedPage() {
+    return (
+        <div>
+            <h1>Accès Refusé</h1>
+            <p>Vous n'avez pas les autorisations nécessaires pour accéder à cette page.</p>
+        </div>
+    );
+}
