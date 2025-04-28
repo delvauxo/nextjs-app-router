feb4cd6028e4f3e00c6f9a856cb8d5dfa6cf3beb
delvauxo <delvauxo@outlook.com>
2025-04-29 00:23:39 +0200

    refactoring

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,22 +1,9 @@
-import postgres from 'postgres';
 import axios from "axios";
 import { formatCurrency } from './utils';
-import {
-  CustomerField,
-  InvoicesTable,
-} from './definitions';
+import { InvoicesTable } from './definitions';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-// Récupère les données de revenu depuis l'API.
-export async function fetchRevenue() {
-  try {
-    const response = await axios.get(`${process.env.API_BASE_URL}/revenue/`);
-    return response.data;
-  } catch (error) {
-    throw new Error("Failed to fetch revenue data.");
-  }
-}
+// Doit correspondre au nombre d'item par page côté backend pour ne pas créer de désalignement.
+const ITEMS_PER_PAGE = 6;
 
 // Récupère et formate les dernières factures (montant et date) depuis l'API.
 export async function fetchLatestInvoices() {
@@ -34,45 +21,6 @@ export async function fetchLatestInvoices() {
     return [];
   }
 }
-
-// Récupère en parallèle le nombre de factures, le nombre de clients et les montants des factures (paid et pending) depuis l'API.
-export async function fetchCardData() {
-  try {
-    const invoiceCountPromise = axios
-      .get(`${process.env.API_BASE_URL}/invoices/count`)
-      .then(response => response.data.count);
-    const customerCountPromise = axios
-      .get(`${process.env.API_BASE_URL}/customers/count`)
-      .then(response => response.data.count);
-    const invoiceStatusPromise = axios
-      .get(`${process.env.API_BASE_URL}/invoices/status`)
-      .then(response => ({ paid: response.data.paid, pending: response.data.pending }));
-
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
-    ]);
-
-    const numberOfInvoices = Number(data[0] ?? '0');
-    const numberOfCustomers = Number(data[1] ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].pending ?? '0');
-
-    return {
-      numberOfInvoices,
-      numberOfCustomers,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    };
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
-  }
-}
-
-// Doit correspondre au nombre d'item par page côté backend pour ne pas créer de désalignement.
-const ITEMS_PER_PAGE = 6;
 
 // Récupère les factures filtrées basée sur la query de la search-bar depuis l'API.
 export async function fetchFilteredInvoices(query: string, currentPage: number): Promise<InvoicesTable[]> {
@@ -140,10 +88,16 @@ export async function fetchCustomers() {
 export async function fetchFilteredCustomers(query: string, currentPage: number) {
   try {
     const response = await axios.get(`${process.env.API_BASE_URL}/customers`, {
-      params: { query, page: currentPage },
+      params: {
+        query,
+        page: currentPage,
+        limit: ITEMS_PER_PAGE,
+      },
       headers: { "Content-Type": "application/json" }
     });
+
     const data = response.data;
+
     // Traitement et formatage des montants avec formatCurrency
     const customers = data.map((customer: any) => ({
       ...customer,
@@ -160,14 +114,58 @@ export async function fetchFilteredCustomers(query: string, currentPage: number)
 // Récupère le nombre total de pages pour les clients filtrés depuis la base de données.
 export async function fetchCustomersPages(query: string): Promise<number> {
   try {
-    const response = await axios.get(`${process.env.API_BASE_URL}/customers/count`, {
-      params: { query }
+    const response = await axios.get(`${process.env.API_BASE_URL}/customers/pages`, {
+      params: { query },
     });
-    const count = Number(response.data.count || 0);
-    const pages = Math.ceil(count / ITEMS_PER_PAGE);
-    return pages;
+    return response.data.totalPages;
   } catch (error) {
     console.error("Failed to fetch customers pages:", error);
     throw new Error("Error fetching pages.");
+  }
+}
+
+// Récupère les données de revenu depuis l'API.
+export async function fetchRevenue() {
+  try {
+    const response = await axios.get(`${process.env.API_BASE_URL}/revenue/`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch revenue data.");
+  }
+}
+
+// Récupère en parallèle le nombre de factures, le nombre de clients et les montants des factures (paid et pending) depuis l'API.
+export async function fetchCardData() {
+  try {
+    const invoiceCountPromise = axios
+      .get(`${process.env.API_BASE_URL}/invoices/count`)
+      .then(response => response.data.count);
+    const customerCountPromise = axios
+      .get(`${process.env.API_BASE_URL}/customers/count`)
+      .then(response => response.data.count);
+    const invoiceStatusPromise = axios
+      .get(`${process.env.API_BASE_URL}/invoices/status`)
+      .then(response => ({ paid: response.data.paid, pending: response.data.pending }));
+
+    const data = await Promise.all([
+      invoiceCountPromise,
+      customerCountPromise,
+      invoiceStatusPromise,
+    ]);
+
+    const numberOfInvoices = Number(data[0] ?? '0');
+    const numberOfCustomers = Number(data[1] ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[2].pending ?? '0');
+
+    return {
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    };
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch card data.');
   }
 }
